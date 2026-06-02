212c9f185c4542da37235f6c107288d2efe69932
Enzo-rar <enzosabatin@gmail.com>
2026-06-02 02:06:14 +0200

    base movement updated

--- NetTest/Assets/Scripts/Player/PlayerMovement.cs
+++ NetTest/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float moveSpeed;
+    private float maxSpeed;
     [Header("Movement")]
-    public float moveSpeed;
-    public float maxSpeed;
+    public float crouchSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float maxWalkSpeed;
+    public float maxSprintSpeed;
+    public float maxSlideSpeed;
+    public float maxCrouchSpeed;
 
     [Header("Jump")]
     public float jumpForce;
@@ -20,19 +27,37 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask whatIsGround;
     bool isGrounded;
 
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
+
     public Transform orientation;
 
     float hInput, vInput;
+    public float crouchYScale, startYScale;
 
     Vector3 moveDirection;
 
     Rigidbody rb;
+
+    public MovementState state;
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air,
+        crouching,
+        sliding
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         readyToJump = true;
+        startYScale = transform.localScale.y;
     }
 
     void Update()
@@ -48,8 +73,11 @@ public class PlayerMovement : MonoBehaviour
         
         MyInput();
         SpeedControl();
-
+        StateHandler();
+        
         Debug.Log("Speed: " + rb.linearVelocity.magnitude);
+
+        
     }
 
     private void FixedUpdate()
@@ -68,35 +96,96 @@ public class PlayerMovement : MonoBehaviour
             Jump();
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - 0.5f, transform.localPosition.z);
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 0.5f, transform.localPosition.z);
+        }
+    }
+
+    private void StateHandler()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+            maxSpeed = maxCrouchSpeed;
+        }
+        else if (isGrounded && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+            maxSpeed = maxSprintSpeed;
+        }
+        else if (isGrounded && !Input.GetKey(KeyCode.LeftControl))
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+            maxSpeed = maxWalkSpeed;
+        }
+        else
+        {
+            state = MovementState.air;
+        }
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * vInput + orientation.right * hInput;
 
-        if (isGrounded)
+        if (OnSlope() && !exitingSlope)
+        {
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+
+            if (rb.angularVelocity.y > 0)
+            {
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+        }
+
+        else if (isGrounded)
         {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         }
-        else
+        else if (!isGrounded)
         {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
         }
+
+        rb.useGravity = !OnSlope();
     }
     
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-
-        if(flatVel.magnitude > maxSpeed)
+        if (OnSlope() && !exitingSlope)
         {
-            Vector3 limitedVel = flatVel.normalized * maxSpeed;
-            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            if (rb.angularVelocity.magnitude > maxSpeed)
+            {
+                rb.angularVelocity = rb.angularVelocity.normalized * maxSpeed;
+            }
+        }
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            if (flatVel.magnitude > maxSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * maxSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
         }
     }
 
     private void Jump()
     {
+        exitingSlope = true;
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
@@ -105,5 +194,23 @@ public class PlayerMovement : MonoBehaviour
     private void ResetJump()
     {
         readyToJump = true;
+
+        exitingSlope = false;
+    }
+
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
     }
 }
